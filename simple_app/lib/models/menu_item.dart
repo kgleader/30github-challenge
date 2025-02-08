3a4b2a3f99762577324154165341a53ebae2f2d2
Meerim <kgleader@gmail.com>
2025-02-07 21:50:54 -0600

    class menuitem changed

--- simple_app/lib/models/menu_item.dart
+++ simple_app/lib/models/menu_item.dart
@@ -1,6 +1,7 @@
 class MenuItem {
   final String name;
   final double price;
+  final String image;
 
-  MenuItem({required this.name, required this.price});
+  MenuItem({required this.name, required this.price, required this.image});
 }
