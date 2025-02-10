8f3863a0fcb64f277bd699659fe6f4fe85175cc5
Meerim <kgleader@gmail.com>
2025-02-09 20:07:55 -0600

    cart options

--- simple_app/lib/screens/menu_screen.dart
+++ simple_app/lib/screens/menu_screen.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/cart.dart';
+import '../screens/cart_screen.dart';
+import '../models/menu_item.dart';
 
 class MenuScreen extends StatelessWidget {
-  const MenuScreen({super.key});
+  final List<MenuItem> menuItems = [
+    MenuItem(name: 'Чай', price: 2.5, image: 'assets/tea.png'),
+    MenuItem(name: 'Кофе', price: 3.0, image: 'assets/coffee.png'),
+    MenuItem(name: 'Пай', price: 4.0, image: 'assets/pie.png'),
+  ];
+
+  MenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var cart = Provider.of<Cart>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Menu Screen'),
+        title: const Text('Меню'),
+        actions: [
+          Stack(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.shopping_cart),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const CartScreen()),
+                  );
+                },
+              ),
+              Positioned(
+                right: 8,
+                top: 8,
+                child: CircleAvatar(
+                  radius: 10,
+                  backgroundColor: Colors.red,
+                  child: Text(
+                    '${cart.itemCount}',
+                    style: const TextStyle(fontSize: 12, color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Welcome to the Menu Screen!'),
+      body: ListView.builder(
+        itemCount: menuItems.length,
+        itemBuilder: (context, index) {
+          MenuItem item = menuItems[index];
+
+          return ListTile(
+            leading: Image.asset(item.image, width: 50, height: 50),
+            title: Text(item.name),
+            subtitle: Text('${item.price} \$'),
+            trailing: ElevatedButton(
+              onPressed: () => cart.addItem(item),
+              child: const Text('Добавить'),
+            ),
+          );
+        },
       ),
     );
   }
